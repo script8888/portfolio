c11f266a2425a6db0becbcf2808f40521281806e
Enoch Fabiyi <ayomikun204@gmail.com>
2022-11-24 02:44:19 +0100

    header nav section done

---
+++ Components/nav/Nav.tsx
@@ -0,0 +1,34 @@
+import React from "react";
+import { GitHub } from "../svg/Svg";
+import styles from "./nav.module.css";
+
+function Nav() {
+  return (
+    <nav className={styles.nav}>
+      <p className={styles.logoName}>Enoch.</p>
+
+      <ul className={styles.navListParent}>
+        <li>
+          <a href="#">Home</a>
+        </li>
+        <li>
+          <a href="#">About</a>
+        </li>
+        <li>
+          <a href="#">Contact</a>
+        </li>
+        <li>
+          <a
+            target="_blank"
+            href="https://github.com/script8888"
+            rel="noreferrer"
+          >
+            <GitHub />
+          </a>
+        </li>
+      </ul>
+    </nav>
+  );
+}
+
+export default Nav;
