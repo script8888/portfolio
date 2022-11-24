b614996325b3e8f847a10ef67f84cb1d29929b25
Enoch Fabiyi <ayomikun204@gmail.com>
2022-11-24 23:14:35 +0100

    project section init

---
+++ Components/Projects/Projects.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+import styles from "./Projects.module.css";
+
+function Projects() {
+  return (
+    <section className={styles.projectsSection}>
+      <div className={styles.projects_heading}>
+        <h3 className={styles.projects_headingH3}>Some Things I’ve Built</h3>
+        <hr className={styles.projects_headingHR} />
+      </div>
+
+      <div className={styles.projects_Grid}></div>
+    </section>
+  );
+}
+
+export default Projects;
