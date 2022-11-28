ffed9b57d51905802fe029448791b831cce66a35
Enoch Fabiyi <ayomikun204@gmail.com>
2022-11-28 21:54:24 +0100

    project dummy details added

--- Components/Projects/ProjectDetails.ts
+++ Components/Projects/ProjectDetails.ts
@@ -1,1 +1,8 @@
-export const projectDetails = [{}, {}];
+export const projectDetails = [
+  {
+    img: "/projects/1.png",
+    title: "100 days of UI",
+    description: `A minimal, dark blue theme for VS Code, Sublime Text, Atom,
+    iTerm, and more.`,
+  },
+];
