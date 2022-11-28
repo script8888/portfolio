86b58bc23de0ab279f9ae8bc8fbb0e2fb3a53fae
Enoch Fabiyi <ayomikun204@gmail.com>
2022-11-29 00:42:50 +0100

    setup chakra ui

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,28 @@
-import "../styles/globals.css";
 import type { AppProps } from "next/app";
+import { ChakraProvider } from "@chakra-ui/react";
+import "../styles/globals.css";
+import { extendTheme } from "@chakra-ui/react";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  const theme = extendTheme({
+    styles: {
+      global: () => ({
+        body: {
+          bg: "",
+          color: "",
+          font: "",
+          fontSize: "",
+          fontFamily: "",
+          fontWeight: "",
+          lineHeight: "",
+        },
+      }),
+    },
+  });
+
+  return (
+    <ChakraProvider theme={theme}>
+      <Component {...pageProps} />
+    </ChakraProvider>
+  );
 }
