5c238651378b76710ea591348584d6fa563d7756
Enoch Fabiyi <ayomikun204@gmail.com>
2023-01-29 17:38:06 +0100

    added id

--- Components/Projects/ProjectDetails.ts
+++ Components/Projects/ProjectDetails.ts
@@ -1,5 +1,6 @@
 export const projectDetails = [
   {
+    id: 1,
     img: "/projects/1.png",
     title: "100 days of UI",
     description: `A minimal, dark blue theme for VS Code, Sublime Text, Atom,
