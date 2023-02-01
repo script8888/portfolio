a7b6a72e30a509531a1da57ec1e228139a878e1a
Enoch Fabiyi <ayomikun204@gmail.com>
2023-02-01 15:55:00 +0100

    added prop types

--- Components/svg/ArrowSide.tsx
+++ Components/svg/ArrowSide.tsx
@@ -1,22 +1,22 @@
-export default function ArrowSide(
-    props: JSX.IntrinsicAttributes & React.SVGProps<SVGSVGElement>
-  ) {
-    return (
-      <svg
-        width={23}
-        height={23}
-        viewBox="0 0 23 23"
-        fill="none"
-        xmlns="http://www.w3.org/2000/svg"
-        {...props}
-      >
-        <path
-          d="M20.567 2.217L2 20.783M20.567 2.217V16.72m0-14.503H6.063"
-          stroke="#C3C3C3"
-          strokeWidth={3.25539}
-          strokeLinecap="round"
-          strokeLinejoin="round"
-        />
-      </svg>
-    );
-  }
+type ArrowSideProps = {};
+
+export const ArrowSide: React.FC<ArrowSideProps> = (props) => {
+  return (
+    <svg
+      width={23}
+      height={23}
+      viewBox="0 0 23 23"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+      {...props}
+    >
+      <path
+        d="M20.567 2.217L2 20.783M20.567 2.217V16.72m0-14.503H6.063"
+        stroke="#C3C3C3"
+        strokeWidth={3.25539}
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+    </svg>
+  );
+};
