58913efa3a42d6bd7ce3f410d72347f3704467ea
Enoch Fabiyi <ayomikun204@gmail.com>
2023-03-22 16:18:50 +0100

    updated

--- Components/Home/Projects/ProjectDetails.ts
+++ Components/Home/Projects/ProjectDetails.ts
@@ -1,9 +1,27 @@
-export const projectDetails = [
+export type projectDetailsType = {
+  id?: number;
+  img: string;
+  title: string;
+  description: string;
+};
+
+export const projectDetails: projectDetailsType[] = [
   {
     id: 1,
-    img: "/projects/1.png",
-    title: "100 days of UI",
-    description: `A minimal, dark blue theme for VS Code, Sublime Text, Atom,
-    iTerm, and more.`,
+    img: "/projects/xjobs.jpeg",
+    title: "XJobs",
+    description: `XJobs is a web app that helps freelancers and clients connect and securely handle payments with the latest web3 technology for a smooth and safe onboarding process.`,
+  },
+  {
+    id: 2,
+    img: "/projects/mobiele.jpeg",
+    title: "Mobiele",
+    description: `Mobiele is a super cool platform based in Africa where you can play mobile games, join tournaments, connect with other gamers, and even win prizes! It's all about gaming fun and competition.`,
+  },
+  {
+    id: 3,
+    img: "/projects/verigo.jpeg",
+    title: "Verigo",
+    description: `Verigo is a company that offers courier/logistics digital solutions for individuals and enterprises in Nigeria and throughout Africa.`,
   },
 ];
