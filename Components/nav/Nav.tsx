5e8af97544ccc51b461394689a0560a5d75517bf
Enoch Fabiyi <ayomikun204@gmail.com>
2023-03-22 16:19:07 +0100

    made name a link

--- Components/nav/Nav.tsx
+++ Components/nav/Nav.tsx
@@ -1,22 +1,22 @@
 import React from "react";
 import GitHub from "../svg/GitHub";
 import styles from "./nav.module.css";
+import Link from "next/link";
 
 function Nav() {
   return (
     <nav className={styles.nav}>
-      <p className={styles.logoName}>Enoch.</p>
+      <Link href="/">
+        <p className={styles.logoName}>Enoch.</p>
+      </Link>
 
       <ul className={styles.navListParent}>
-        <li>
+        {/* <li>
           <a href="#">Home</a>
         </li>
         <li>
           <a href="#">About</a>
-        </li>
-        <li>
-          <a href="#">Contact</a>
-        </li>
+        </li> */}
         <li>
           <a
             target="_blank"
