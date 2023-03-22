abfe15c59e8025ea9f40fb71c5e8c456e2845e80
Enoch Fabiyi <ayomikun204@gmail.com>
2023-03-22 16:19:50 +0100

    projects looking good I guess

--- Components/Home/Projects/index.tsx
+++ Components/Home/Projects/index.tsx
@@ -1,17 +1,20 @@
 import React from "react";
 import styles from "./Projects.module.css";
 import { SimpleGrid } from "@chakra-ui/react";
-import { projectDetails } from "./ProjectDetails";
+import { projectDetails, projectDetailsType } from "./ProjectDetails";
+import Link from "next/link";
 
-const Project = (props: any) => {
+const Project = (props: projectDetailsType) => {
   return (
-    <div key={props.id} className={styles.project}>
-      <img src={props.img} alt="1" />
-      <div className={styles.hoverDetails}>
-        <h3>{props.title}</h3>
-        <p>{props.description}</p>
+    <Link href={`/case-study/${props.title.toLowerCase()}`}>
+      <div className={styles.project}>
+        <img src={props.img} alt="1" />
+        <div className={styles.hoverDetails}>
+          <h3>{props.title}</h3>
+          <p>{props.description}</p>
+        </div>
       </div>
-    </div>
+    </Link>
   );
 };
 
@@ -24,18 +27,8 @@ function Projects() {
       </div>
 
       <div className={styles.projects_GridParent}>
-        <SimpleGrid columns={{ sm: 2, md: 3, lg: 3 }} spacing={7}>
+        <SimpleGrid columns={{ sm: 1, md: 2, lg: 2 }} spacing={7}>
           {projectDetails.map(Project)}
-          <div className={styles.project}>
-            <img src="/projects/1.png" alt="1" />
-            <div className={styles.hoverDetails}>
-              <h3>100 days of UI</h3>
-              <p>
-                A minimal, dark blue theme for VS Code, Sublime Text, Atom,
-                iTerm, and more.
-              </p>
-            </div>
-          </div>
         </SimpleGrid>
       </div>
     </section>
