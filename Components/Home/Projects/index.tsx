60279f38174274f56565b9946f75dc630b7826f3
Enoch Fabiyi <ayomikun204@gmail.com>
2023-04-12 00:03:07 +0100

    animatoin applied

--- Components/Home/Projects/index.tsx
+++ Components/Home/Projects/index.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import styles from "./Projects.module.css";
 import { projectDetails, projectDetailsType } from "./ProjectDetails";
 import Link from "next/link";
+import { motion } from "framer-motion";
 
 const Project = (props: projectDetailsType) => {
 	return (
@@ -9,13 +10,23 @@ const Project = (props: projectDetailsType) => {
 			target={props.url ? "_blank" : "_self"}
 			href={props.url ? props.url : `/case-study/${props.title.toLowerCase()}`}
 		>
-			<div className={styles.project}>
+			<motion.div
+				initial={{ opacity: 0, scale: 0.9 }}
+				whileInView={{ opacity: 1, scale: 1 }}
+				transition={{
+					delay: props.id ? 0.5 + props.id * 0.17 : 0.5,
+					duration: 1.5,
+					type: "tween",
+				}}
+				viewport={{ once: true, amount: 0.7 }}
+				className={styles.project}
+			>
 				<img src={props.img} alt="1" />
 				<div className={styles.hoverDetails}>
 					<h3>{props.title}</h3>
 					<p>{props.description}</p>
 				</div>
-			</div>
+			</motion.div>
 		</Link>
 	);
 };
@@ -24,8 +35,22 @@ function Projects() {
 	return (
 		<section id="projects" className={styles.projectsSection}>
 			<div className="heading">
-				<h3 className="headingH3">Some Things I’ve Built</h3>
-				<hr className={`headingHR`} />
+				<motion.h3
+					initial={{ opacity: 0, x: -20 }}
+					whileInView={{ opacity: 1, x: 0 }}
+					transition={{ delay: 0.4, duration: 0.8, type: "tween" }}
+					viewport={{ once: true, amount: 0.7 }}
+					className="headingH3"
+				>
+					Some Things I’ve Built
+				</motion.h3>
+				<motion.hr
+					initial={{ opacity: 0, x: 15 }}
+					whileInView={{ opacity: 1, x: 0 }}
+					transition={{ delay: 0.5, duration: 1, type: "tween" }}
+					viewport={{ once: true, amount: 0.7 }}
+					className={`headingHR`}
+				/>
 			</div>
 
 			<div className={styles.projects_GridParent}>
