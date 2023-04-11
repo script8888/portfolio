771640dc9c59c135888dcc6ba1208527d9153df9
Enoch Fabiyi <ayomikun204@gmail.com>
2023-04-12 00:03:00 +0100

    animaton applied

--- Components/Home/HeadSection/index.tsx
+++ Components/Home/HeadSection/index.tsx
@@ -3,6 +3,7 @@ import React from "react";
 import Nav from "../../nav/Nav";
 import ArrowDown from "../../svg/ArrowDown";
 import styles from "./headSection.module.css";
+import { motion } from "framer-motion";
 
 function HeadSection() {
 	return (
@@ -10,20 +11,21 @@ function HeadSection() {
 			<Nav />
 
 			<div className={styles.head_details}>
-				<h3 className={styles.head_name}>
+				<motion.h3
+					initial={{ opacity: 0, y: -20 }}
+					animate={{ opacity: 1, y: 0 }}
+					transition={{ delay: 0.7, duration: 0.8, type: "tween" }}
+					className={styles.head_name}
+				>
 					<span className={styles.head_name_MainText}>ENOCH FABIYI</span>{" "}
 					<span className={styles.head_name_OutlineText}>ENOCH FABIYI</span>
-				</h3>
+				</motion.h3>
 				<p className={styles.head_desc}>
 					Experienced Frontend Web Developer with 3+ years of expertise in
 					building dynamic and user-friendly web applications using
 					NextJs/ReactJs, JavaScript, MongoDB.
 				</p>
 
-				{/* <div className={styles.letsTalkDiv}>
-          <p>I’m currently accepting oppotunities,</p>
-          <a className="hrefs" href="mailto:ayomikun204@gmail.com">LET’S TALK.</a>
-        </div> */}
 				<div className={styles.arrowDownDiv}>
 					<Link href="/#projects">
 						<ArrowDown />
