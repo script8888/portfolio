75aad3d81ac5054f668ae3c552463987a8c83a8b
Enoch Fabiyi <ayomikun204@gmail.com>
2023-04-12 00:01:22 +0100

    removed chakra setup

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,28 +1,6 @@
 import type { AppProps } from "next/app";
-import { ChakraProvider } from "@chakra-ui/react";
 import "../styles/globals.css";
-import { extendTheme } from "@chakra-ui/react";
 
 export default function App({ Component, pageProps }: AppProps) {
-  const theme = extendTheme({
-    styles: {
-      global: () => ({
-        body: {
-          bg: "",
-          color: "",
-          font: "",
-          fontSize: "",
-          fontFamily: "",
-          fontWeight: "",
-          lineHeight: "",
-        },
-      }),
-    },
-  });
-
-  return (
-    <ChakraProvider theme={theme}>
-      <Component {...pageProps} />
-    </ChakraProvider>
-  );
+	return <Component {...pageProps} />;
 }
