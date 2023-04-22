57f79bff677a9060ef71430509aeb4b61df9640b
Enoch Fabiyi <ayomikun204@gmail.com>
2023-04-22 19:13:36 +0100

    added script tag to document

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -10,6 +10,20 @@ function Document() {
           href="https://fonts.googleapis.com/css2?family=Aleo:wght@700&family=Public+Sans:wght@300;400;600;700;800;900&family=Source+Code+Pro&display=swap"
           rel="stylesheet"
         />
+        <script
+					async
+					src="https://www.googletagmanager.com/gtag/js?id=G-12GLBZBC2L"
+				></script>
+				<script
+					dangerouslySetInnerHTML={{
+						__html: `window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+          
+            gtag('config', 'G-12GLBZBC2L');`,
+					}}
+				>
+				</script>
       </Head>
       <body>
         <Main />
