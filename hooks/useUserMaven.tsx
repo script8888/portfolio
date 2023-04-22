07c858ce739d9193adb2414f7e7185251b162b0b
Enoch Fabiyi <ayomikun204@gmail.com>
2023-04-22 17:24:34 +0100

    user maven hook

---
+++ hooks/useUserMaven.tsx
@@ -0,0 +1,26 @@
+import { useState, useEffect } from "react";
+import { usermavenClient, UsermavenClient } from "@usermaven/sdk-js";
+const useUserMaven = () => {
+	const [usermaven, setUserMaven] = useState<UsermavenClient | null>(null);
+
+	useEffect(() => {
+		// Initialize UsermavenClient when component mounts
+		const usermavenInstance: UsermavenClient = usermavenClient({
+			key: process.env.NEXT_PUBLIC_USERMAVEN_KEY as string,
+			tracking_host: "https://events.usermaven.com",
+		});
+
+		// Set UsermavenClient instance to state
+		setUserMaven(usermavenInstance);
+
+		// Clean up when component unmounts
+		return () => {
+			// Perform any necessary cleanup for UsermavenClient instance
+			// (e.g. unsubscribe event listeners, close connections, etc.)
+		};
+	}, []);
+
+	return usermaven;
+};
+
+export default useUserMaven;
