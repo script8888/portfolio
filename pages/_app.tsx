0690dff7b3d6536fe19d25544899de2d0f8152fb
Enoch Fabiyi <ayomikun204@gmail.com>
2023-04-22 19:27:08 +0100

    removed hotjat

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,6 @@
 import type { AppProps } from "next/app";
 import "../styles/globals.css";
-import Hotjar from "@hotjar/browser";
 
-const siteId = 3462209;
-const hotjarVersion = 6;
-
-Hotjar.init(siteId, hotjarVersion);
 export default function App({ Component, pageProps }: AppProps) {
 	return <Component {...pageProps} />;
 }
