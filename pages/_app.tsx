0dca32c8cf05d75d11d7d3cffcb19414ecec2d47
Enoch Fabiyi <ayomikun204@gmail.com>
2023-04-22 19:18:10 +0100

    npm hotjar install

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,11 @@
 import type { AppProps } from "next/app";
 import "../styles/globals.css";
+import Hotjar from "@hotjar/browser";
 
+const siteId = 3462209;
+const hotjarVersion = 6;
+
+Hotjar.init(siteId, hotjarVersion);
 export default function App({ Component, pageProps }: AppProps) {
 	return <Component {...pageProps} />;
 }
