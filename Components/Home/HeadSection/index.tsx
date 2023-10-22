d2dd93c7c94b211a83ed8843a4dcbaf5ca4f8203
Enoch Fabiyi <ayomikun204@gmail.com>
2023-10-22 12:56:27 +0100

    updated head desc

--- Components/Home/HeadSection/index.tsx
+++ Components/Home/HeadSection/index.tsx
@@ -6,34 +6,33 @@ import styles from "./headSection.module.css";
 import { motion } from "framer-motion";
 
 function HeadSection() {
-	return (
-		<section className={styles.HeadSection}>
-			<Nav />
+  return (
+    <section className={styles.HeadSection}>
+      <Nav />
 
-			<div className={styles.head_details}>
-				<motion.h3
-					initial={{ opacity: 0, y: -20 }}
-					animate={{ opacity: 1, y: 0 }}
-					transition={{ delay: 0.7, duration: 0.8, type: "tween" }}
-					className={styles.head_name}
-				>
-					<span className={styles.head_name_MainText}>ENOCH FABIYI</span>{" "}
-					<span className={styles.head_name_OutlineText}>ENOCH FABIYI</span>
-				</motion.h3>
-				<p className={styles.head_desc}>
-					Experienced Fullstack Developer with 3+ years of expertise in
-					building dynamic and user-friendly web applications using
-					NextJs/ReactJs, JavaScript, MongoDB.
-				</p>
+      <div className={styles.head_details}>
+        <motion.h3
+          initial={{ opacity: 0, y: -20 }}
+          animate={{ opacity: 1, y: 0 }}
+          transition={{ delay: 0.7, duration: 0.8, type: "tween" }}
+          className={styles.head_name}
+        >
+          <span className={styles.head_name_MainText}>ENOCH FABIYI</span>{" "}
+          <span className={styles.head_name_OutlineText}>ENOCH FABIYI</span>
+        </motion.h3>
+        <p className={styles.head_desc}>
+          Experienced Developer on deck to deliver solutions using React,
+          NextJs, TypeScript, Javascript, Firebase, and MongoDB.
+        </p>
 
-				<div className={styles.arrowDownDiv}>
-					<Link href="/#projects">
-						<ArrowDown />
-					</Link>
-				</div>
-			</div>
-		</section>
-	);
+        <div className={styles.arrowDownDiv}>
+          <Link href="/#projects">
+            <ArrowDown />
+          </Link>
+        </div>
+      </div>
+    </section>
+  );
 }
 
 export default HeadSection;
